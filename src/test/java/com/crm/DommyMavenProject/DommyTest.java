5ae4c5f848b81791f72b856cf73d805007a2ef1f
Indrajitm96 <moharanaindrajit@gmail.com>
2022-07-25 12:55:28 +0530

    commit-1

---
+++ src/test/java/com/crm/DommyMavenProject/DommyTest.java
@@ -0,0 +1,10 @@
+package com.crm.DommyMavenProject;
+
+class DommyTest {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("Our new President Draupadi Murmu");
+	}
+
+}
